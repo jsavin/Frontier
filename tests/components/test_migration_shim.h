8c3845566f18ebbce0f9396aada8dbff06164f2f
Jake Savin <jsavin@gmail.com>
2025-11-30 22:11:57 -0800

    Disable portable stubs for test_migration; drop strings_portable include

--- tests/components/test_migration_shim.h
+++ tests/components/test_migration_shim.h
@@ -2,8 +2,6 @@
 #define TEST_MIGRATION_SHIM_H
 
 #include "../../portable/file_portable.h"
-#include "../../portable/strings_portable.h"
-
 boolean test_copy_file(const char *src, const char *dst);
 void test_remove_if_exists(const char *path);
 
